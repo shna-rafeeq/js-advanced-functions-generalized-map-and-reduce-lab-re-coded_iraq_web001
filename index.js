2e67e44c38a40fddd7a811746f49558f41e61905
shna rafeeq <shnar.majeed@gmail.com>
2020-06-10 18:40:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your functions here
+function map(array, callBack) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    newArray.push(callBack(element));
+  }
+  return newArray;
+}
+
+
+function reduce(array, callBack, startingPoint) {
+  let val;
+  if (startingPoint !== undefined) {
+    val = startingPoint;
+    for (const element of array) {
+      val = callBack(val, element);
+    }
+  } else {
+    val = array[0];
+    array = array.slice(1);
+    for (const element of array) {
+      val = callBack(val, element);
+    }
+  }
+  return val;
+}
