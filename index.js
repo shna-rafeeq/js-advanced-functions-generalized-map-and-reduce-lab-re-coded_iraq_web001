ed6314e4180a8f482ec0cac9e912a478453ea397
shna rafeeq <shnar.majeed@gmail.com>
2020-06-10 18:40:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // Add your functions here
-
+function map(array, callBack) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    newArray.push(callBack(element));
+  }
+  return newArray;
+}
